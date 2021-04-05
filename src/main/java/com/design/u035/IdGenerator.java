be3999df619d84bf585ef56710841c676a921007
leojin <jinlb@qq.com>
2021-04-05 17:30:50 +0800

    feat(算法生成): 添加单元测试
    添加单元测试

--- src/main/java/com/design/u035/IdGenerator.java
+++ src/main/java/com/design/u035/IdGenerator.java
@@ -1,5 +1,13 @@
 package com.design.u035;
 
+/**
+ * @author yjs
+ */
 public interface IdGenerator {
+    /**
+     * idGenerator生成接口
+     *
+     * @return {@link String}
+     */
     String generator();
 }
--- src/main/java/com/design/u035/LogTraceIdGenerator.java
+++ src/main/java/com/design/u035/LogTraceIdGenerator.java
@@ -1,4 +1,7 @@
 package com.design.u035;
 
+/**
+ * @author yjs
+ */
 public interface LogTraceIdGenerator extends IdGenerator {
 }
